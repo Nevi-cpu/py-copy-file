c8bd18e5feecea037a1348a01ebf4736b32ab6af
Andrii Usypiv <anriyusypiv@gmail.com>
2026-02-17 14:10:05 +0200

    R2

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ def copy_file(command: str) -> Any:
     parts = command.split()
     source = parts[1]
     destination = parts[2]
-    if source == destination: return
+    if source == destination:
+        return
     with open(source, "r") as file_in, open(destination, "w") as file_out:
         file_out.write(file_in.read())
