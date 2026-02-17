ca07201c9a01190301659ea219b0be38c0493af7
Andrii Usypiv <anriyusypiv@gmail.com>
2026-02-17 14:08:08 +0200

    R

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+from typing import Any
+
+
+def copy_file(command: str) -> Any:
+    parts = command.split()
+    source = parts[1]
+    destination = parts[2]
+    if source == destination: return
+    with open(source, "r") as file_in, open(destination, "w") as file_out:
+        file_out.write(file_in.read())
